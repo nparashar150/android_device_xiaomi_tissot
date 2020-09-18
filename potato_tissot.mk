230558cf26028aa2d0ae5b463a10a4ba2b9c9d9e
nparashar150 <nparashar150@gmail.com>
2020-09-18 21:29:55 +0530

    POSP Tree

--- potato_tissot.mk
+++ potato_tissot.mk
@@ -21,12 +21,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from tissot device
 $(call inherit-product, device/xiaomi/tissot/device.mk)
 
-# Inherit some common DerpFest OS stuff.
-$(call inherit-product, vendor/colt/config/common_full_phone.mk)
+# Inherit some common POSP stuff.
+$(call inherit-product, vendor/potato/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := tissot
-PRODUCT_NAME := colt_tissot
+PRODUCT_NAME := potato_tissot
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Mi A1
@@ -34,12 +34,8 @@ PRODUCT_MANUFACTURER := Xiaomi
 TARGET_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-TARGET_GAPPS_ARCH := arm64
-TARGET_BOOT_ANIMATION_RES := 1080x1920
-IS_PHONE := true
 
-COLT_BUILD_TYPE := OFFICIAL
-COLT_DEVICE_MAINTAINER := Naman_Parashar
+IS_PHONE := true
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="tissot-user 8.0.0 OPR1.170623.026 8.1.10 release-keys"
 
