f5dc3b01e90c5240909451f8288da3390d8392e9
nparashar150 <nparashar150@gmail.com>
2020-09-18 21:33:55 +0530

    added script for running if needed

---
+++ posp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash/
+
+git clone https://github.com/nparashar150/android_device_xiaomi_tissot.git -b POSP device/xiaomi/tissot/
+git clone https://github.com/nparashar150/android_device_xiaomi_msm8953-common.git device/xiaomi/msm8953-common/
+git clone https://github.com/MASTERGUY/kernel_xiaomi_tissot.git kernel/xiaomi/tissot/
+git clone https://github.com/MASTERGUY/kernel_xiaomi_tissot.git kernel/xiaomi/msm8953/
+git clone https://github.com/MASTERGUY/vendor_xiaomi_tissot.git vendor/xiaomi/
+
+
